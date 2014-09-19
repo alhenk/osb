bbc419d2afa65788c9d56e70b0df3b410950c0c4
alhen <alexandr.g.koryagin@gmail.com>
2014-09-19 14:48:46 +0600

    initial commit

---
+++ xml-beans-util/src/com/epam/koryagin/xmlbeans/Runner.java
@@ -0,0 +1,55 @@
+package com.epam.koryagin.xmlbeans;
+
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlObject;
+import org.apache.xmlbeans.XmlOptions;
+import com.osb.cits.kz.test.registry.RequestDocument;
+
+public class Runner {
+	public static void main(String[] args) throws XmlException {
+		RequestDocument requestDocument;
+		XmlObject requestData;
+		XmlOptions opts;
+
+		/*1 Receive XML REQUEST*/
+		requestDocument = RequestDocument.Factory.parse(XmlUtil.REQUEST);
+		/*2 Retrieve Data (message) from XML REQUEST*/
+		requestData = XmlUtil.retrieveRequestData(requestDocument);
+		opts = new XmlOptions();
+		opts.setSavePrettyPrint();
+		opts.setSavePrettyPrintIndent(4);
+		System.out
+				.println("REQUEST DATA:\n"
+						+ (null == requestData ? "NO DATA" : requestData
+								.xmlText(opts)));
+
+		/*3 Put Data (message) into Queue and add header attribute and JMSPriority*/
+		long priority = XmlUtil.getJMSPriority(requestData);
+		System.out.println("JMS PRIORITY = " + priority);
+				
+		/*4 Send Response with Status OK/FAIL */
+		XmlObject response = XmlUtil.createResponse("OK");
+		System.out.println("RESPONSE:\n"
+				+ (null == response ? "NO RESPONSE" : response.xmlText(opts)));
+		
+		/*5 Consume the message from Queue with the attribute selector (messageType = 'messageMy')*/
+		/*6 Replace name space, remove prefixes 'test' and change attribute 'class' value on '06'*/
+		XmlObject data = requestData.copy();
+		data = XmlUtil.setDataClazz(data, "29");
+		XmlObject message = data.copy();
+		message = XmlUtil.removeNamespaces(message);
+		System.out.println("MESSAGE:\n"
+				+ (null == message ? "NO MESSAGE" : message.xmlText(opts)));
+		
+		/*7 Add the message into the Info, get UUID*/
+		/*8 Print out the Info in log */
+		XmlObject info = XmlUtil.createInfo(message);
+		System.out.println("INFO:\n"
+				+ (null == info ? "NO INFO" : info.xmlText(opts)));
+		
+		/*9 Send the Info into Proxy3 and retrieve the message */
+		XmlObject messageProxy = XmlUtil.retrieveInfoMessage(info)
+;		System.out.println("MESSAGE PROXY3:\n"
+				+ (null == message ? "NO MESSAGE PROXY3" : messageProxy.xmlText(opts)));
+	}
+}
---
+++ xml-beans-util/src/com/epam/koryagin/xmlbeans/XmlUtil.java
@@ -0,0 +1,198 @@
+package com.epam.koryagin.xmlbeans;
+
+import java.util.UUID;
+
+import javax.xml.namespace.QName;
+
+import kz.test.osbservice.InfoDocument;
+import kz.test.osbservice.InfoType;
+import kz.test.osbservice.MessageType;
+
+import noNamespace.ResponseDocument;
+import noNamespace.ResponseType;
+
+import org.apache.xmlbeans.XmlCursor;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlObject;
+import org.apache.xmlbeans.XmlOptions;
+
+import com.osb.cits.kz.test.registry.DataDocument;
+import com.osb.cits.kz.test.registry.DataType;
+import com.osb.cits.kz.test.registry.RequestDocument;
+import com.osb.cits.kz.test.registry.RequestType;
+
+public class XmlUtil {
+	public static final String STATUS_OK = "OK";
+	public static final String STATUS_FAIL = "FAIL";
+	public static final long MIN_JMSPRIORITY = 1L;
+	public static final long MAX_JMSPRIORITY = 9L;
+	public static final String TEST_NAMESPACE = "http://kz.cits.osb.com/test/registry";
+	public static final String DATA_ELEMENT = "data";
+
+	public static final String INFO = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+			+ "<Info xmlns=\"http://osbservice.test.kz/\">"
+			+ "<Name>InfoName</Name>"
+			+ "<Class>InfoClass</Class>"
+			+ "<UUID>545e417e-3d5b-11e4-8782-164230d1df67</UUID>" + "</Info>";
+
+	public static final String REQUEST = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+			+ "<test:request xmlns:test=\"http://kz.cits.osb.com/test/registry\">"
+			+ "<test:data clazz=\"05\">"
+			+ "<test:title>Serendipity</test:title>"
+			+ "<test:director>Peter Chelsom</test:director>"
+			+ "<test:release>MMI</test:release>"
+			+ "<test:rating>2</test:rating>"
+			+ "<test:grade>3</test:grade>"
+			+ "</test:data>" + "</test:request>";
+
+	public static final String DATA = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+			+ "<test:data clazz=\"05\" xmlns:test=\"http://kz.cits.osb.com/test/registry\">"
+			+ "<test:title>Serendipity</test:title>"
+			+ "<test:director>Peter Chelsom</test:director>"
+			+ "<test:release>MMI</test:release>"
+			+ "<test:rating>12</test:rating>"
+			+ "<test:grade>3</test:grade>"
+			+ "</test:data>";
+
+	public static final String BAD_REQUEST = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+			+ "<test:request xmlns:test=\"http://kz.cits.osb.com/test/registry\">"
+			+ "</test:request>";
+	public static final String RESPONSE = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+			+ "<response>" + "</response>";
+
+	public static XmlObject retrieveRequestData(XmlObject xml) {
+		RequestDocument requestDocument;
+		DataType dataType;
+		XmlObject data;
+		XmlOptions opt;
+		try {
+			requestDocument = RequestDocument.Factory.parse(xml.xmlText());
+			// requestDocument = (RequestDocument) xml.copy();
+			RequestType request = requestDocument.getRequest();
+			dataType = request.getData();
+			if (dataType == null) {
+				return null;
+			}
+			opt = new XmlOptions();
+			opt.setSaveSyntheticDocumentElement(new QName(TEST_NAMESPACE,
+					DATA_ELEMENT));
+			data = XmlObject.Factory.parse(dataType.xmlText(opt));
+
+		} catch (XmlException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return data;
+	}
+
+	public static long getJMSPriority(XmlObject xml) {
+		long rating = 0L;
+		long grade = 0L;
+		long priority = MIN_JMSPRIORITY;
+		// DataDocument dataDocument = (DataDocument) xml;
+		DataDocument dataDocument;
+		try {
+			dataDocument = DataDocument.Factory.parse(xml.xmlText());
+		} catch (XmlException e) {
+			e.printStackTrace();
+			return MIN_JMSPRIORITY;
+		}
+		DataType data = dataDocument.getData();
+		if (data != null) {
+			rating = (long) data.getRating();
+			grade = (long) data.getGrade();
+		}
+		priority = rating + grade;
+		if (priority < 1)
+			priority = MIN_JMSPRIORITY;
+		if (priority > 9)
+			priority = MAX_JMSPRIORITY;
+		return priority;
+	}
+
+	public static XmlObject createResponse(String status) {
+		ResponseDocument responseDocument = null;
+		try {
+			responseDocument = ResponseDocument.Factory.parse(RESPONSE);
+			ResponseType response = responseDocument.getResponse();
+			response.setStatus(status);
+		} catch (XmlException e) {
+			e.printStackTrace();
+		}
+		return responseDocument;
+	}
+
+	public static XmlObject createInfo(XmlObject xml) {
+		InfoDocument infoDocument;
+		InfoType infoType;
+		XmlCursor cursor = null;
+		try {
+			MessageType messageType = MessageType.Factory.parse(xml.xmlText());
+			infoDocument = InfoDocument.Factory.parse(XmlUtil.INFO);
+			infoType = infoDocument.getInfo();
+			cursor = infoType.newCursor();
+			while (!cursor.toNextToken().isNone()) {
+				if (cursor.currentTokenType().intValue() == XmlCursor.TokenType.INT_START
+						&& cursor.getName().getLocalPart().equals("UUID")) {
+					cursor.setTextValue(UUID.randomUUID().toString()
+							.toUpperCase());
+				}
+			}
+			infoType.setMessage(messageType);
+		} catch (XmlException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			if (cursor != null) {
+				cursor.dispose();
+			}
+		}
+		return infoDocument;
+	}
+
+	public static XmlObject setDataClazz(XmlObject xml, String clazzValue) {
+
+		XmlCursor cursor = xml.newCursor();
+		while (!cursor.toNextToken().isNone()) {
+			if (cursor.isAttr()
+					&& cursor.getName().getLocalPart().equals("clazz")) {
+				cursor.setTextValue(clazzValue);
+			}
+		}
+		return xml;
+	}
+
+	public static XmlObject retrieveInfoMessage(XmlObject xml) {
+		// InfoDocument infoDocument = (InfoDocument) xml;
+		InfoDocument infoDocument;
+		try {
+			infoDocument = InfoDocument.Factory.parse(xml.xmlText());
+		} catch (XmlException e) {
+			e.printStackTrace();
+			return null;
+		}
+		InfoType info = infoDocument.getInfo();
+		MessageType message = info.getMessage();
+		return message;
+	}
+
+	public static XmlObject removeNamespaces(XmlObject xml) {
+		XmlObject root = xml.copy();
+		String localPart;
+		XmlCursor cursor = root.newCursor();
+		cursor.toNextToken();
+		while (cursor.hasNextToken()) {
+			if (cursor.isNamespace()) {
+				cursor.removeXml();
+			} else {
+				if (cursor.isStart() || cursor.isAttr()) {
+					localPart = cursor.getName().getLocalPart();
+					cursor.setName(new QName(localPart));
+				}
+				cursor.toNextToken();
+			}
+		}
+		cursor.dispose();
+		return root;
+	}
+}
